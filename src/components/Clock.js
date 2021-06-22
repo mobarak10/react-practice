048a3da8fe5ae764f252824f0facc0a60624d782
mobarak10 <47492246+mobarak10@users.noreply.github.com>
2021-06-22 08:53:43 +0600

    work in state, setState, componentDidMount

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,10 +3,23 @@
 import React from 'react';
 
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        setInterval(() => this.tick(), 1000);
+    }
+
+    tick() {
+        this.setState({ date: new Date() });
+    }
+
     render() {
         return (
             <h1 className="heading">
-                <span>{new Date().toLocaleTimeString(this.props.locale)}</span>
+                <span>{this.state.date.toLocaleTimeString(this.props.locale)}</span>
             </h1>
         );
     }
