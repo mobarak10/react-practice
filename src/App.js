537af2848924bbfdc8b647d8ee09c0ce1a8531df
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2022-08-22 19:38:48 +0600

    class 21 done & practice UserCallback, useMemo & memo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React, { useState } from 'react';
-import MyComponent from './components/MyComponent';
+import React, { useCallback, useMemo, useState } from 'react';
+import Button from './components/Button';
+import ShowCount from './components/ShowCount';
+import Title from './components/Title';
 
 export default function App() {
-    const [show, setShow] = useState(true);
+    const [count1, setCount1] = useState(0);
+    const [count2, setCount2] = useState(0);
+
+    const incrementByOne = useCallback(() => {
+        setCount1((prevState) => prevState + 1);
+    }, []);
+
+    const incrementByFive = useCallback(() => {
+        setCount2((prevState) => prevState + 5);
+    }, []);
+
+    const evenOrOdd = useMemo(() => {
+        let i = 0;
+        while (i < 1000000000) {
+            i += 1;
+        }
+
+        return count1 % 2 === 0;
+    }, [count1]);
+
     return (
         <div className="app">
-            <div>{show && <MyComponent />}</div>
-            <p>
-                <button type="button" onClick={() => setShow((prevState) => !prevState)}>
-                    {show ? 'Hide Post' : 'Show Post'}
-                </button>
-            </p>
+            <Title />
+            <ShowCount count={count1} title="Counter 1" />
+            <span>{evenOrOdd ? 'Even' : 'Odd'}</span>
+            <Button handleClick={incrementByOne}>Increment By One</Button>
+            <hr />
+            <ShowCount count={count2} title="Counter 2" />
+            <Button handleClick={incrementByFive}>Increment By Five</Button>
         </div>
     );
 }
