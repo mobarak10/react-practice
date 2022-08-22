44b1e0dd8c591990b8f52383e49781a90bbcdd38
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2022-08-22 21:31:54 +0600

    class 22 Done practice useRef & forwardRef

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-import React, { useCallback, useMemo, useState } from 'react';
-import Button from './components/Button';
-import ShowCount from './components/ShowCount';
-import Title from './components/Title';
+import React from 'react';
+// import Form from './components/Form';
+import Time from './components/Time';
 
 export default function App() {
-    const [count1, setCount1] = useState(0);
-    const [count2, setCount2] = useState(0);
-
-    const incrementByOne = useCallback(() => {
-        setCount1((prevState) => prevState + 1);
-    }, []);
-
-    const incrementByFive = useCallback(() => {
-        setCount2((prevState) => prevState + 5);
-    }, []);
-
-    const evenOrOdd = useMemo(() => {
-        let i = 0;
-        while (i < 1000000000) {
-            i += 1;
-        }
-
-        return count1 % 2 === 0;
-    }, [count1]);
-
     return (
         <div className="app">
-            <Title />
-            <ShowCount count={count1} title="Counter 1" />
-            <span>{evenOrOdd ? 'Even' : 'Odd'}</span>
-            <Button handleClick={incrementByOne}>Increment By One</Button>
-            <hr />
-            <ShowCount count={count2} title="Counter 2" />
-            <Button handleClick={incrementByFive}>Increment By Five</Button>
+            {/* <Form /> */}
+            <Time />
         </div>
     );
 }
